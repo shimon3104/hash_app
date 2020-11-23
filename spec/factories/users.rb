cdb78d27529862467d25d1e67f9083e922b063a4
shimon3104 <shimon.aida@gmail.com>
2020-11-23 17:13:52 +0900

    Userモデル単体テスト

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :user do
-    
+    nickname              { Faker::Name.initials }
+    email                 { Faker::Internet.free_email }
+    password              { 'test123' }
+    password_confirmation { password }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,69 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#create' do
+    before do
+      @user = FactoryBot.build(:user)
+    end
+
+    context '新規登録がうまくいく時' do
+      it '全ての項目が存在すれば登録できる' do
+        expect(@user).to be_valid
+      end
+
+      it 'passwordが6文字以上であれば登録できる' do
+        @user.password = 'aaaaaa'
+        @user.password_confirmation = 'aaaaaa'
+        @user.valid?
+      end
+    end
+
+    context '新規登録がうまくいかない時' do
+      it 'nicknameが空では登録できない' do
+        @user.nickname = nil
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Nickname can't be blank")
+      end
+
+      it 'emailが空では登録できない' do
+        @user.email = nil
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Email can't be blank")
+      end
+
+      it 'passwordが空では登録できない' do
+        @user.password = nil
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password can't be blank")
+      end
+
+      it '重複したemailが存在する場合登録できない' do
+        @user.save
+        another_user = FactoryBot.build(:user, email: @user.email)
+        another_user.valid?
+        expect(another_user.errors.full_messages).to include('Email has already been taken')
+      end
+
+      it 'emailに@がない場合は登録できない' do
+        @user.email = 'aaa.com'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Email is invalid')
+      end
+
+      it 'passwordが5文字以下であれば登録できない' do
+        @user.password = 'aaaaa'
+        @user.password_confirmation = 'aaaaa'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
+      end
+
+      it 'passwordとpassword_confirmationが不一致では登録できない' do
+        @user.password = 'aaaaaa'
+        @user.password_confirmation = 'bbbbbb'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      end
+      
+    end
+  end
 end
