af7e3d0ab65b29c027894bd0eb8f498ff15886e9
shimon3104 <shimon.aida@gmail.com>
2020-11-23 20:13:35 +0900

    Notes_tag単体テスト、Noteモデル単体テスト修正

--- spec/factories/note_tag_relations.rb
+++ spec/factories/note_tag_relations.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :note_tag_relation do
-    
+    association :note
+    association :tag
   end
 end
--- spec/factories/notes.rb
+++ spec/factories/notes.rb
@@ -1,7 +1,5 @@
 FactoryBot.define do
   factory :note do
-    title  { Faker::Lorem.characters(number: 50, min_numeric: 1) }
-    text { Faker::Lorem.sentence }
     association :user
   end
 end
